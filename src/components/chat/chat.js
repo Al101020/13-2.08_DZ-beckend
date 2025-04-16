3f06789a8f682560a549ed1e61618dd07afee837
Al101020 <alex.vl.m@yandex.ru>
2025-04-17 00:09:02 +0300

    commit-1.6

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,3 +1,5 @@
+import './chat.css';
+
 const body = document.querySelector('body');
 
 function chat() {
@@ -25,11 +27,11 @@ function chat() {
   divChat.appendChild(messages);
 
   const inputMessage = document.createElement('div');
-  inputMessage.classList.add('inputMessage');
+  inputMessage.classList.add('input-message');
   const form = document.createElement('form');
-  form.classList.add('formInput');
+  form.classList.add('form-input');
   const input = document.createElement('input');
-  input.classList.add('inputText');
+  input.classList.add('input-text');
   input.type = 'text';
   form.appendChild(input);
   inputMessage.appendChild(form);
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,16 +1,33 @@
 import './modal.css';
 
 const body = document.querySelector('body');
+
 const fullScreen = document.createElement('div');
-fullScreen.classList.add('fullScreen');
+fullScreen.classList.add('full-screen');
 body.appendChild(fullScreen);
+
 const divModal = document.createElement('div');
 divModal.classList.add('div-modal');
+
 const modal = document.createElement('div');
 modal.classList.add('modal');
 const title = document.createElement('div');
 title.classList.add('title-modal');
 title.textContent = 'Выберете псевдоним';
 modal.appendChild(title);
+
+const formModal = document.createElement('form');
+formModal.classList.add('form-input');
+const inputModal = document.createElement('input');
+inputModal.classList.add('input-text-modal');
+inputModal.type = 'text';
+formModal.appendChild(inputModal);
+const btnModal = document.createElement('button');
+btnModal.classList.add('btn-modal');
+btnModal.textContent = 'Продолжить';
+formModal.appendChild(btnModal);
+
+modal.appendChild(formModal);
+
 divModal.appendChild(modal);
 body.appendChild(divModal);
